f90c16c4da0ecf6b4bd2bc7d46522c361e71718e
fahryanputra <fahryanputra@gmail.com>
2023-09-14 13:31:07 +0700

    ganti config

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    DB_URL: str
+    DB_URL: str = 'postgresql://postgres:postgres@localhost:5432/substation_planning'
 
     class Config:
         env_file = ".env"
